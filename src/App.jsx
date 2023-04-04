ba6a85d7e2e9fda5825a76a129c303fcb79265c9
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-04 15:26:05 +0600

    Changed the title of the project

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,6 @@ export default function App() {
     <div className="App">
       <h1>Hello world</h1>
       <p>This is my diploma</p>
-      <p>hello world</p>
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, eaque! Ipsam maiores quos numquam voluptatum earum, a, deleniti inventore doloribus placeat illum nihil consequuntur velit ea, reiciendis quam quae voluptates.</p>
-      <h2>test</h2>
-      
-    </div>
     </div>
   );
 }
