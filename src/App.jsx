161789975791f41a9edb0d3e63cbb610cf7dd58f
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-04 15:30:39 +0600

    Added a new picture

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ export default function App() {
     <div className="App">
       <h1>Hello world</h1>
       <p>This is my diploma</p>
+
+      <img src="https://thumbs.dreamstime.com/b/baby-panda-tree-chengdu-research-base-giant-breeding-30774508.jpg" />
     </div>
   );
 }
