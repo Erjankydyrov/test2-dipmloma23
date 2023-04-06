e993fd0ef61a969fb71029b698e2d397d1024845
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-06 14:17:26 +0600

    Add Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import { NavLink, Route, Routes } from "react-router-dom";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      <p>This is my diploma</p>
-
       <nav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
@@ -13,7 +10,7 @@ export default function App() {
 
       <main>
         <Routes>
-          <Route path="/" element={<h2>Home</h2>} exact />
+          <Route path="/" element={<h2>Home</h2>} />
           <Route path="/about" element={<h2>About</h2>} />
         </Routes>
       </main>
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,9 @@
+import "./Layout.css";
+
+export default function Layout(props) {
+  return (
+    <div className="Layout">
+      
+    </div>
+  );
+}
