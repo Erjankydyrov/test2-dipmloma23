5b0dcf18fa244f0347a9018ab096d90c3018bd03
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-11 16:21:46 +0600

    Add category list from firebase

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./CategoryList.css";
+import { getDocs } from "firebase/firestore";
+import { categoryCollection } from "../../firebase";
+import { NavLink } from "react-router-dom";
 
 export default function CategoryList() {
   const [categories, setCategories] = useState([]);
+
+  // выполнить эту функцию только один раз
+  useEffect(() => {
+    // получить категории из списка категорий
+    getDocs(categoryCollection)
+      .then(snapshot => {
+        // категории будут храниться в snapshot.docs
+
+        // создать массив для категорий
+        const newCategories = [];
+        // заполнить массив данными из списка категорий
+        snapshot.docs.forEach(doc => { // doc = категория
+          const category = doc.data();
+          category.id = doc.id;
+
+          newCategories.push(category);
+        });
+        // задать новый массив как состояние комапо
+        setCategories(newCategories);
+      })
+  }, []);
 
   const output = categories.map(category => (
     <li key={category.id}>
