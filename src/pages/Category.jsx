abc7a14e502098ae51bc55c0d8008c2d26e840f4
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-13 15:08:11 +0600

    Show category name

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,7 +1,33 @@
+import { useContext } from "react";
+import { useMatch } from "react-router-dom";
+import { AppContext } from "../App";
+
 export default function Category() {
+  // деструктизацтия
+  const { params } = useMatch("/category/:path");
+  const { categories } = useContext(AppContext);
+  // простой метод
+  // const match = useMatch("/category/:path");
+  // match.params.path;
+
+  // найти категорию где путь категории совпадает с путем в браузере
+  const category = categories.find(
+    category => params.path === category.path,
+    /*
+    function(category) {
+      if (params.path === category.path) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    */
+  );
+
   return (
     <div className="Category">
-      <h1>Category</h1>
+      <h1>{category.name}</h1>
     </div>
   );
 }
